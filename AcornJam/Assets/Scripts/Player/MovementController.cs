36d5432825979b2540a6d73387d17de776d2fb50
Yaygun <yusufaygunnn@hotmail.com>
2024-04-12 02:24:10 +0300

    Smooth look rotation

--- AcornJam/Assets/Scripts/Player/MovementController.cs
+++ AcornJam/Assets/Scripts/Player/MovementController.cs
@@ -13,11 +13,13 @@ public class MovementController : MonoBehaviour
     public Vector2 inputDirection { get; private set; }
     [SerializeField] float CameraHeightDif;
 
+    Vector2[] lookArr = new Vector2[3] { new Vector2(0, 0), new Vector2(0, 0), new Vector2(0, 0) };
+
 
     void Start()
     {
         inputRecieve.MoveF = Movement;
-        inputRecieve.LookF = Rotate;
+        inputRecieve.LookF = GetLook;
     }
 
     // Update is called once per frame
@@ -33,15 +35,32 @@ public class MovementController : MonoBehaviour
         CurrentSpeed = moveDir.magnitude;
         cc.Move(moveDir * Speed * Time.deltaTime);
     }
+
+    void GetLook(Vector2 direction)
+    {
+        GetNewLookDir(direction);
+        Rotate(CalculateDirection());
+    }
     void Rotate(Vector2 direction)
     {
         transform.Rotate(Vector3.up * direction.x * LookSpeed  );
         cam.transform.Rotate(Vector3.left * direction.y * LookSpeed);
     }
 
-    void CameraHeight()
+    Vector2 CalculateDirection()
     {
+        Vector2 v = new Vector2();
+        v += lookArr[0] * 0.33f;
+        v += lookArr[1] * 0.33f;
+        v += lookArr[2] * 0.33f;
+        return v;
+    }
 
+    void GetNewLookDir(Vector2 dir)
+    {
+        lookArr[0] = lookArr[1];
+        lookArr[1] = lookArr[2];
+        lookArr[2] = dir;
     }
 
     
